eca70c796a469b2fe07d6c5447609ecd3b7f5b80
amoldalwai <amoldalwaiyt@gamil.com>
2020-09-27 23:58:34 +0530

    Updates

---
+++ precache-manifest.abf2bb785063a40a623fcbfc08166510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3f74b87ec9d06798be0",
+    "url": "/quotesApp/static/css/main.f7f8d6fb.chunk.css"
+  },
+  {
+    "revision": "e3f74b87ec9d06798be0",
+    "url": "/quotesApp/static/js/main.7bb45f54.chunk.js"
+  },
+  {
+    "revision": "871b32068367b0ec2752",
+    "url": "/quotesApp/static/js/runtime~main.dcde7c86.js"
+  },
+  {
+    "revision": "86336ca833c7fec7317f",
+    "url": "/quotesApp/static/css/2.a408fa18.chunk.css"
+  },
+  {
+    "revision": "86336ca833c7fec7317f",
+    "url": "/quotesApp/static/js/2.c1f6e5f0.chunk.js"
+  },
+  {
+    "revision": "d934af4031d01d8feaa22bce9dc9a6d8",
+    "url": "/quotesApp/index.html"
+  }
+];
